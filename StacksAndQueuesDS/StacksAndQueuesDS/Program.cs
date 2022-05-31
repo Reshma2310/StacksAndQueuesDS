9d0d5154e5428baa979f72d32a5b96a2763a8641
Reshma2310 <dreshma253@gmail.com>
2022-05-31 09:55:51 +0500

    [Reshma] - Creating Stack Using Push

---
+++ StacksAndQueuesDS/StacksAndQueuesDS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StacksAndQueuesDS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            StackLinkedList stack = new StackLinkedList();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.Display();
+        }
+    }
+}
